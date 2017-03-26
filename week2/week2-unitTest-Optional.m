af43f4b0df409fcf6a3bbed4319b0fae76f69da3
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-03-26 10:46:04 -0300

    Update top level comments with default gnu license

--- week2/week2-unitTest-Optional.m
+++ week2/week2-unitTest-Optional.m
@@ -1,8 +1,17 @@
-## Copyright
+## Copyright (C) 2017 Machine Learning Coursera - March 2017
 ##
-## Machine Learning Coursera - March 2017
-## by Alexandro de Oliveira
-## email: alexandro.oliveira@holbertonschool.com
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ##
@@ -13,6 +22,9 @@
 ## >> test week2-unitTest-Optional verbose % if you want more details about error/success
 ##
 ## Enjoy!
+##
+## Author: Alexandro <alexandro.oliveira@holbertonschool.com>
+## Created: 2017-03-25
 
 %% Optional tasks/functions
 
