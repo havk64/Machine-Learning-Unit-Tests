4b2939843dd9feafd5e00df3560f2b2548d237f7
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-03-26 10:18:33 -0300

    Transfer tests for feature normalization for optional tests

--- week2/week2-unitTest-Optional.m
+++ week2/week2-unitTest-Optional.m
@@ -16,6 +16,34 @@
 
 %% Optional tasks/functions
 
+%% Test for featureNormalize function
+%% Test case 1
+%!shared Xn, mu, sigma, exp, tol
+%! [Xn mu sigma] = featureNormalize([1; 2; 3]);
+%! exp = [-1;0;1];
+%! tol = eps;
+%! fprintf('Testing featureNormalize function...\n');
+%!assert(Xn, exp);
+%!assert(mu, 2);
+%!assert(sigma, 1);
+%% Test case 2
+%!shared Xn, mu, sigma, exp, tol
+%! [Xn mu sigma] = featureNormalize(magic(3));
+%! exp = [1.13389 -1.00000 0.37796; -0.75593 0.00000 0.75593; -0.37796 1.00000 -1.13389];
+%! tol = -10^14*eps;
+%!assert(Xn, exp, tol);
+%!assert(mu, [5 5 5]);
+%!assert(sigma,[2.6548 4.0000 2.6458], tol);
+%% Test case 3
+%!shared Xn, mu, sigma, exp, tol
+%! [Xn mu sigma] = featureNormalize([-ones(1,3); magic(3)]);
+%! exp = [-1.21725 -1.01472 -1.21725;...
+%! 1.21725 -0.56373 0.67625; -0.13525 0.33824 0.94675; 0.13525 1.24022 -0.40575];
+%! tol = -10^11*eps;
+%!assert(Xn, exp, tol);
+%!assert(mu, [3.5 3.5 3.5]);
+%!assert(sigma,[3.6968 4.4347 3.6968], tol);
+
 %% Test for computeCostMulti function
 %% Test case 1
 %!shared X, y, theta, expected, tol
