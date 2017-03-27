78de91270a42cf8ec0e1de839d60f80eecf29554
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-03-27 19:55:54 -0300

    Add warning messages as the tests succeds

--- week3/ex2_unit_tests.m
+++ week3/ex2_unit_tests.m
@@ -31,12 +31,14 @@
 %! tol = 11^10*eps;
 %! m_exp = [0.99966 0.73106 0.99753; 0.95257 0.99331 0.99909; 0.98201 0.99988 0.88080];
 %! e_exp = [0.73106 0.5; 0.5 0.73106];
+%! fprintf('Testing sigmoid function... ');
 %!assert(sigmoid(1200000), 1);
 %!assert(sigmoid(-25000), 0);
 %!assert(sigmoid(0), 0.5);
 %!assert(sigmoid([4 5 6]), [0.98201, 0.99331, 0.99753], tol);
 %!assert(sigmoid(eye(2)), e_exp, tol);
 %!assert(sigmoid(magic(3)),m_exp, tol);
+%! fprintf('PASSED!\n');
 
 %% Test for costFunction(Unregularized)
 %!shared X, y, theta, J, grad, tol
@@ -45,8 +47,10 @@
 %! theta = [-2 -1 1 2]';
 %! [J grad] = costFunction(theta, X, y);
 %! tol = 10^12*eps;
+%! fprintf('Testing costFunction... ');
 %!assert(J, 4.6832, tol);
 %!assert(grad, [0.31722 0.87232 1.64812 2.23787]', tol);
+%! fprintf('PASSED!\n');
 
 %% Test for predict function
 %% Test case 1
@@ -55,12 +59,15 @@
 %! theta = [-3.5 ; 1.3];
 %! expected = logical([0 0 1 1])';
 %!assert(predict(theta, X), expected);
+%! fprintf('Testing predict function... 1,');
 %% Test case 2
 %!shared X, theta, expected
 %! X = magic(3);
 %! theta = [4 3 -8]';
 %! expected = logical([0 0 1])';
+%! fprintf('2 ');
 %!assert(predict(theta,X), expected);
+%! fprintf('PASSED!\n');
 
 %% Test for costFunctionReg(Regularized)
 %% Test case 1
@@ -70,6 +77,7 @@
 %! theta = [-2 -1 1 2]';
 %! [J grad] = costFunctionReg(theta, X, y, 0);
 %! tol = 14^10*eps;
+%! fprintf('Testing costFunctionReg... 1,');
 %!assert(J, 4.6832, tol);
 %!assert(grad, [0.31722 0.87232 1.64812 2.23787]', tol);
 %% Test case 2
@@ -79,5 +87,7 @@
 %! theta = [-2 -1 1 2]';
 %! [J grad] = costFunctionReg(theta, X, y, 4);
 %! tol = 14^10*eps;
+%! fprintf('2 ');
 %!assert(J, 8.6832, tol);
 %!assert(grad, [0.31722 -0.46102 2.98146 4.90454]', tol);
+%! fprintf('PASSED!\n');
