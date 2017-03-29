c5bae412466f9a381da20c4bbed934868b111b3e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-03-29 19:54:33 -0300

    Increasing float precision, status messages

--- week3/ex2_unit_tests.m
+++ week3/ex2_unit_tests.m
@@ -28,17 +28,22 @@
 
 %% Test for warmUpExercise Sigmoid function
 %!shared tol, m_exp, e_exp
-%! tol = 11^10*eps;
+%! tol = -12^10*eps;
 %! m_exp = [0.99966 0.73106 0.99753; 0.95257 0.99331 0.99909; 0.98201 0.99988 0.88080];
 %! e_exp = [0.73106 0.5; 0.5 0.73106];
-%! fprintf('Testing sigmoid function... ');
+%! fprintf('Testing sigmoid function:\n');
 %!assert(sigmoid(1200000), 1);
+%! fprintf('  - Test 1 OK!\n');
 %!assert(sigmoid(-25000), 0);
+%! fprintf('  - Test 2 OK!\n');
 %!assert(sigmoid(0), 0.5);
+%! fprintf('  - Test 3 OK!\n');
 %!assert(sigmoid([4 5 6]), [0.98201, 0.99331, 0.99753], tol);
+%! fprintf('  - Test 4 OK!\n');
 %!assert(sigmoid(eye(2)), e_exp, tol);
+%! fprintf('  - Test 5 OK!\n');
 %!assert(sigmoid(magic(3)),m_exp, tol);
-%! fprintf('OK!!!\n');
+%! fprintf('  - Test 6 OK! => PASSED!\n');
 
 %% Test for costFunction(Unregularized)
 %!shared X, y, theta, J, grad, tol
@@ -46,11 +51,12 @@
 %! y = [1 0 1]';
 %! theta = [-2 -1 1 2]';
 %! [J grad] = costFunction(theta, X, y);
-%! tol = 10^12*eps;
-%! fprintf('Testing costFunction... ');
+%! tol = -12^10*eps;
+%! fprintf('Testing costFunction:\n');
 %!assert(J, 4.6832, tol);
+%! fprintf('  - Cost function OK!\n');
 %!assert(grad, [0.31722 0.87232 1.64812 2.23787]', tol);
-%! fprintf('OK!!!\n');
+%! fprintf('  - Gradient OK! => PASSED!\n');
 
 %% Test for predict function
 %% Test case 1
@@ -58,16 +64,16 @@
 %! X = [1 1 ; 1 2.5 ; 1 3 ; 1 4];
 %! theta = [-3.5 ; 1.3];
 %! expected = logical([0 0 1 1])';
+%! fprintf('Testing predict function:\n');
 %!assert(predict(theta, X), expected);
-%! fprintf('Testing predict function... 1,');
+%! fprintf('  - Test 1 OK!\n');
 %% Test case 2
 %!shared X, theta, expected
 %! X = magic(3);
 %! theta = [4 3 -8]';
 %! expected = logical([0 0 1])';
-%! fprintf('2 ');
 %!assert(predict(theta,X), expected);
-%! fprintf('OK!!!\n');
+%! fprintf('  - Test 2 OK! => PASSED!\n');
 
 %% Test for costFunctionReg(Regularized)
 %% Test case 1
@@ -76,18 +82,20 @@
 %! y = [1 0 1]';
 %! theta = [-2 -1 1 2]';
 %! [J grad] = costFunctionReg(theta, X, y, 0);
-%! tol = 14^10*eps;
-%! fprintf('Testing costFunctionReg... 1,');
+%! tol = -12^10*eps;
+%! fprintf('Testing costFunctionReg:\n');
 %!assert(J, 4.6832, tol);
+%! fprintf('  - Cost function Test 1 OK!\n');
 %!assert(grad, [0.31722 0.87232 1.64812 2.23787]', tol);
+%! fprintf('  - Gradient Test 1 OK!\n');
 %% Test case 2
 %!shared X, y, theta, J, grad, tol
 %! X = [ones(3,1) magic(3)];
 %! y = [1 0 1]';
 %! theta = [-2 -1 1 2]';
 %! [J grad] = costFunctionReg(theta, X, y, 4);
-%! tol = 14^10*eps;
-%! fprintf('2 ');
+%! tol = -12^10*eps;
 %!assert(J, 8.6832, tol);
+%! fprintf('  - Cost function Test 2 OK!\n');
 %!assert(grad, [0.31722 -0.46102 2.98146 4.90454]', tol);
-%! fprintf('OK!!!\n');
+%! fprintf('  - Gradient test 2 OK! => PASSED!\n');
