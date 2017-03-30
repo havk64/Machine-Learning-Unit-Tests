17252c6b133bcbde11f3876ce29119b6b1c753c1
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-03-30 18:43:39 -0300

    Improved texinfo syntax, move license to separate file

--- week4/ex3_nn_unit_tests.m
+++ week4/ex3_nn_unit_tests.m
@@ -1,30 +1,22 @@
 ## Copyright (C) 2017 Machine Learning Coursera - March 2017
 ##
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## Author: Alexandro <alexandro.oliveira@holbertonschool.com>
+## Created: 2017-03-29
 
 ## -*- texinfo -*-
 ##
 ## Unit test for week 4, Programming assignment
+##
 ## Put this file in the ex3/ folder and execute it from Octave with:
+##
 ## >> test ex3_nn_unit_tests
-## or
-## >> test ex3_nn_unit_tests verbose % if you want more details about error/success
+##
+## or, if you want more details about error/success
+##
+## >> test ex3_nn_unit_tests verbose
 ##
 ## Enjoy!
 ##
-## Author: Alexandro <alexandro.oliveira@holbertonschool.com>
-## Created: 2017-03-29
 
 %% Test for predict function
 %!shared Theta1, Theta2, X, expected
