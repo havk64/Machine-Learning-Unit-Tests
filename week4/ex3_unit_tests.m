b7b4b9e15174cc68c0899f0e735009f6e61c8611
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-03-30 03:17:25 -0300

    Fix typos, improved output

--- week4/ex3_unit_tests.m
+++ week4/ex3_unit_tests.m
@@ -15,8 +15,8 @@
 
 ## -*- texinfo -*-
 ##
-## Unit test for week 3, Programming assignment - Mandatory functions
-## Put this file in the ex1/ folder and execute it from Octave with:
+## Unit test for week 3, Programming assignment
+## Put this file in the ex3/ folder and execute it from Octave with:
 ## >> test ex3_unit_tests
 ## or
 ## >> test ex3_unit_tests verbose % if you want more details about error/success
@@ -34,12 +34,12 @@
 %! lambda = 3;
 %! [J grad] = lrCostFunction(theta, X, y, lambda);
 %! tol = -12^10*eps;
-%! fprintf('Testing lsCostFunction function:\n');
+%! fprintf('Testing lrCostFunction function...\n');
 %!assert(J, 2.5348, tol);
 %! fprintf(' - Cost Function OK!\n');
 %!assert(grad, [ 0.14656 -0.54856 0.72472 1.39800]', tol);
 %! fprintf(' - Gradient OK!\n');
-%! fprintf('lrCostFunction => PASSED!\n');
+%! fprintf('Test on lrCostFunction => PASSED!\n');
 
 %% Test for oneVsAll function
 %!shared X, y, num_labels, lambda, expected, tol
@@ -51,14 +51,14 @@
 %! -5.4738  -0.471565   1.2613   0.63482; ...
 %! 0.068368  -0.375582  -1.652262  -1.410138];
 %! tol = -12^10*eps(2);
-%! fprintf('Testing oneVsAll function:\n');
+%! fprintf('Testing oneVsAll function...\n');
 %!assert(oneVsAll(X, y, num_labels, lambda), expected, tol);
-%! fprintf('Function oneVsAll => PASSED!\n');
+%! fprintf('Test on Function oneVsAll => PASSED!\n');
 
 % Test for predictOneVsAll function
 %!shared all_theta, X
 %! X = [1 7; 4 5; 7 8; 1 4];
 %! all_theta = [1 -6 3; -2 4 -3];
-%! fprintf('Testing predictOneVsAll function:\n');
+%! fprintf('Testing predictOneVsAll function...\n');
 %!assert(predictOneVsAll(all_theta, X),[1 2 2 1]');
-%! fprintf('predictOneVsAll function => PASSED!\n');
+%! fprintf('Test on predictOneVsAll function => PASSED!\n');
